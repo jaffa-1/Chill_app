031f32de41b7e4887167161ef27c2442102d22c6
jaffa-1 <pranet.v08@gmail.com>
2026-01-02 12:40:31 +0530

    buttons pop

---
+++ Assets/_Scripts/ButtonSingleUI.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSingleUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    Vector3 shrinkScale = Vector3.one;
+    Vector3 expandScale = new Vector3(1.1f,1.1f,1.1f);
+
+    RectTransform rectTransform;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+        float timer = 0;
+        float timerMax = 0.5f;
+
+        while(timer < timerMax)
+        {
+            timer += Time.deltaTime;
+            rectTransform.localScale = Vector3.Lerp(shrinkScale, expandScale, timer*timer);
+        }
+
+        rectTransform.localScale = expandScale;
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+        float timer = 0;
+        float timerMax = 0.75f;
+
+        while (timer < timerMax)
+        {
+            timer += Time.deltaTime;
+            rectTransform.localScale = Vector3.Lerp(expandScale, shrinkScale, timer*timer);
+        }
+
+        rectTransform.localScale = shrinkScale;
+    }
+}
